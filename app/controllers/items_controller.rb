603a37f33a62ee0cc484081ae575b16cf4f5f675
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-05-07 18:23:10 +0300

    Complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all.includes(:user)
+        render json: items.map { |item| item.as_json(include: :user) }
+      end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        items = user.items
+        render json: {
+          id: user.id,
+          username: user.username,
+          city: user.city,
+          items: items.as_json(only: [:id, :name, :description, :price])
+        }
+      end
 end
